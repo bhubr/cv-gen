aff6b6c61f18e3fa52ba7501cc0c4e74172ac08c
Benoît Hubert <benoithubert@gmail.com>
2021-03-01 10:09:07 +0100

    Process base data & skills

--- index.js
+++ index.js
@@ -2,64 +2,61 @@ const frontmatter = require('@github-docs/frontmatter')
 const ejs = require('ejs')
 const { join } = require('path');
 const fs = require('fs');
-const getTemplates = require('./tmpl');
+const getTemplates = require('./lib/tmpl');
+const getExpDetails = require('./lib/mdast');
+const { expSchema } = require('./lib/schemas');
+const { dataDir } = require('./lib/config');
+const { readFile, readJsonFile } = require('./lib/utils');
+const readExperiences = require('./lib/read-experiences');
+
 const latexTemplateDir = join(__dirname, 'cv-template');
 const latexSectionsDir = join(latexTemplateDir, 'cv-sections');
 
-const expSchema = {
-  properties: {
-    title: {
-      type: 'string',
-      required: true
-    },
-    organization: {
-      type: 'string',
-      required: true
-    },
-    location: {
-      type: 'string',
-      required: true
-    },
-    dates: {
-      type: 'string',
-      required: true
-    },
+function processBaseData() {
+  const baseFile = join(dataDir, 'main.json');
+  const base = readJsonFile(baseFile);
+  const { position: posArr, ...rest } = base;
+  const position = posArr.join('{\\enskip\\cdotp\\enskip}');
+  const data = { ...rest, position };
+  const cvTmpl = getTemplates('resume_cv');
+  const mainSection = ejs.render(cvTmpl, data);
+  fs.writeFileSync(join(latexTemplateDir, 'resume_cv.tex'), mainSection);
+}
+
+function processSkills() {
+  const skillsFile = join(dataDir, 'skills.md');
+  const skillsMd = readFile(skillsFile);
+  const skills = getExpDetails(skillsMd)
+    .map(line => {
+      const [cat, items] = line.split('|');
+      return { cat, items }
+    });
+
+  const skillsTmpl = getTemplates('skills')
+  const skillsSection = ejs.render(skillsTmpl, { skills })
+  fs.writeFileSync(join(latexSectionsDir, 'skills.tex'), skillsSection);
+}
+
+function processExperiences() {
+  const exps = readExperiences();
+
+  const formatExp = (markdown) => {
+    const { data, errors } = frontmatter(markdown, { schema: expSchema })
+    if (errors.length > 0) throw new Error(`Error in ${markdown.substr(0, 20)}`)
+    return data
   }
+
+  const experiences = exps.map(md => {
+    const data = formatExp(md)
+    const details = getExpDetails(md)
+    return { ...data, details }
+  })
+
+  const expTmpl = getTemplates('experience')
+  const expSection = ejs.render(expTmpl, { experiences })
+  fs.writeFileSync(join(latexSectionsDir, 'experience.tex'), expSection);
 }
 
-const exps = [
-`---
-title: JavaScript instructor & content editor
-organization: Wild Code School
-location: Toulouse (FR) / Remote
-dates: Feb. 2018 - Apr. 2021
----
-
-* Teach web development fundamentals: front-end (HTML, CSS, JS, React), back-end (Node.js, Express, SQL).
-* Supervise student projects, esp. end-of-term projects with real clients.
-* Create and improve teaching materials for the JavaScript curriculum.
-`,
-`---
-title: Web developer
-organization: Freelance
-location: Toulouse (FR)
-dates: Apr. 2016 - Nov. 2017
----
-
-* TextBeans, web marketplace for translation and correction of English texts, (Node.js/Express, AngularJS).
-* WordPress websites for small companies in Toulouse and Rennes.
-* Contributions to Red Pesto / ElsieApp, a web-based language school management solution (PHP, Laravel, MySQL).
-`
-]
-
-const formatExp = (markdown) => {
-  const { data, content, errors } = frontmatter(markdown, { schema: expSchema })
-  console.log(errors)
-  if (errors.length > 0) throw new Error(`Error in ${markdown.substr(0, 20)}`)
-  return { data, content }
-}
-
-const experiences = exps.map(md => formatExp(md))
-const expTmpl = getTemplates('experience')
-const expSection = ejs.render(expTmpl, { experiences })
-fs.writeFileSync(join(latexSectionsDir, 'experience.tex'), expSection);
+processSkills();
+processBaseData();
+processExperiences();
